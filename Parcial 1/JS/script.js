e0d4f5062a5279aa87718c3dc8f1eb11e196f4b8
Laboratorio 103 <estudiante103@unicamacho.co>
2025-03-06 20:59:16 -0500

    Mi parcial

---
+++ Parcial 1/JS/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('miBoton').addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com/invites/contact/?igsh=hdigy2pq4tta&utm_content=wb7sup5';
+});
+
